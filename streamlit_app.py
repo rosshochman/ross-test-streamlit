188df8e3b10da33725b5cee90cbb172560669891
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-03-28 22:42:01 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,25 +37,23 @@ def fetch_data():
     return df_sorted
 
 def main():
-    df1 = st.empty()
+
 
 # Infinite loop to continuously update data
+    while True:
+        try:
+            # Fetch data from Polygon.io API
+            new_df1 = fetch_data()
 
-    try:
-        # Fetch data from Polygon.io API
-        new_df1 = fetch_data()
+            # Display data frames
+            st.header('Stocks')
+            st.dataframe(new_df1)
 
-        # Display data frames
-        st.header('Stocks')
-        st.dataframe(new_df1)
+            # Sleep for 1 second before making the next API call
+            time.sleep(10)
 
-        df1 = new_df1
-
-        # Sleep for 1 second before making the next API call
-        time.sleep(10)
-
-    except Exception as e:
-        pass
+        except Exception as e:
+            pass
 
 # Run the Streamlit app
 if __name__ == '__main__':
